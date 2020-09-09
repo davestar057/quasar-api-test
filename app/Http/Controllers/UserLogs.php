e4a4a447236916766093702c7847511b82265981
davidhaw <david@kabaddle.com>
2020-09-09 18:35:27 +0100

    update logs sort order

--- app/Http/Controllers/UserLogs.php
+++ app/Http/Controllers/UserLogs.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 class UserLogs extends Controller
 {
     public function index(){
-
-        return ['data' => ActivityLog::all()];
+        return [ 'data' => ActivityLog::orderByDesc('created_at')->get() ];
     }
 }
